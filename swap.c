23041b188a474e98c50545c876703eb203e1d108
ryannewmanGithub <RyanNewman280@gmail.com>
2025-01-01 15:35:31 -0800

    Explain more about incorrect swap

--- swap.c
+++ swap.c
@@ -2,6 +2,8 @@
 
 // This is incorrect. Do NOT do this because this only affects the local copy of the integers a and b.
 // Instead, you need to pass in the pointers.
+// Another way is to create a temporary variable, set that equal to b. Then set b = a. Then set a = temp.
+// However, that would use additional memory, which is constrained in embedded systems, for example.
 void incorrectSwap(int a, int b) {
     a = a + b;
     b = a - b; // (a + b) - b = a
